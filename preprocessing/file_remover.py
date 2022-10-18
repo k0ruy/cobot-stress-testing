0de3de99df315b6eaccbf46e56369defa4a85487
Christian Pala <pala.christian@gmail.com>
2022-10-18 10:15:28 +0200

    Removed 06-05-30_10-53-13_manual from questionnaires.pkl and added a duplicate row for patient 10

--- preprocessing/file_remover.py
+++ preprocessing/file_remover.py
@@ -3,17 +3,29 @@
 # Libraries:
 import pickle
 from pathlib import Path
+import pandas as pd
 
 
 # Driver:
 
 if __name__ == '__main__':
-    with open(Path('..', 'questionnaires.pkl'), 'rb') as f:
+    # Load the questionnaires pickle:
+    with open(Path('..', "questionnaires.pkl"), "rb") as f:
         subject_data = pickle.load(f)
 
-        # drop row 73:
-        subject_data.drop(73, inplace=True)
+        # remove the row with index 73 as it is the row with the file 06-05-30_10-53-13_manual:
+        subject_data = subject_data.drop(subject_data.iloc[73].name)
 
-        # save the new pickle file:
-        pickle.dump(subject_data, open(Path('..', 'questionnaires.pkl'), 'wb'))
+        # duplicate all rows with user ID 10:
+        subject_11 = subject_data[subject_data['ID'] == 10].copy()
+
+        # change the ID to 11:
+        subject_11['ID'] = 11
+
+        # concatenate the two dataframes:
+        subject_data = pd.concat([subject_data, subject_11], axis=0)
+
+    with open(Path('..', 'questionnaires.pkl'), 'wb') as f:
+        pickle.dump(subject_data, f)
+
 
