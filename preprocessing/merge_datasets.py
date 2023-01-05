d6d5b92c5b7f2c0e67e1ee765ba002f002f89c06
C. Berchtold <christian.berchtold@student.supsi.ch>
2023-01-05 19:35:14 +0100

    preoprocessing

--- preprocessing/merge_datasets.py
+++ preprocessing/merge_datasets.py
@@ -10,20 +10,22 @@ def merge_all_patients_csv_files():
     """
     Merge all the csv files of all patients by task into one csv file
     """
-    # Get all the csv files, exclude the stress labels, recursively:
-    csv_files = [os.path.join(root, f) for root, dirs, files in os.walk(SAVED_DATA) for f in files]
-
-    # Get the tasks:
-    tasks = ['cobot', 'manual', 'rest', 'stroopeasy', 'stroophard']
+    # Supported tasks:
+    tasks = ['cobot', 'manual']
+    signals = ['ecg', 'eda', 'emg']
 
     for task in tasks:
-        # get all the files with this task
-        task_files = [f for f in csv_files if task in f]
+        for signal in signals:
+            # Get all the csv files of the current task and signal:
+            csv_files = list((SAVED_DATA.rglob(f'{signal}_{task}*.csv')))
+            print(csv_files)
 
-        dataframes = [pd.read_csv(f) for f in task_files]
+            # Create a list of dataframes and concatenate them:
+            dataframes = [pd.read_csv(csv_file) for csv_file in csv_files]
 
-        # save the file
-        dataframes[0].to_csv(os.path.join(SAVED_DATA, f'merged_{task}.csv'), index=False)
+            df = pd.concat(dataframes)
+            # Save the merged dataframe:
+            df.to_csv(SAVED_DATA / f'{signal}.csv', index=False)
 
 
 if __name__ == '__main__':
